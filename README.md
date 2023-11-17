176237267b173cd82021eb20cac53990cec52f28
OlegPichkur <oleg1pichkur@gmail.com>
2023-11-17 10:05:09 +0200

    commit 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Site 4 --ArchiWeb--
+
+### Верстка сайта  
+#### Используемые технологии: HTML, CSS, SCSS, Flex, Grid, JS, Perfect Pixel .  
+Боковое меню бургера реализовано с использованием framework - Materialize
---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+   /*инициализация интерактивного меню*/
+   var sidenav = document.querySelectorAll('.sidenav');
+   var instancesSidenav = M.Sidenav.init(sidenav);
+   /*инициализация модального окна*/
+   // var modal = document.querySelectorAll('.modal');
+   // var instancesModal = M.Modal.init(modal);
+   // /*инициализация галереи*/   
+   // var boxed = document.querySelectorAll('.materialboxed');
+   // var instancesBoxed = M.Materialbox.init(boxed);
+ });
+/*-----------Функции-------------*/
+
+
+
+/*---------События------------*/
+
+
+
+ 
