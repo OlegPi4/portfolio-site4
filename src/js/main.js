f39af94d72367b65d1af2d2f97bb839dd4122262
OlegPichkur <oleg1pichkur@gmail.com>
2023-11-17 20:14:54 +0200

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
    var sidenav = document.querySelectorAll('.sidenav');
    var instancesSidenav = M.Sidenav.init(sidenav);
    /*инициализация модального окна*/
-   // var modal = document.querySelectorAll('.modal');
-   // var instancesModal = M.Modal.init(modal);
+   var modal = document.querySelectorAll('.modal');
+   var instancesModal = M.Modal.init(modal);
    /*инициализация галереи*/   
    var boxed = document.querySelectorAll('.materialboxed');
    var instancesBoxed = M.Materialbox.init(boxed);
